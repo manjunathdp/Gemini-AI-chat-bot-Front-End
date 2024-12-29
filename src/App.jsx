c8879398017e60f293dd201c16107baf5e0ef289
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-29 21:46:22 +0530

    ChatRequest component updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
 
 import './App.css'
+import ChatRequest from './components/ChatRequest';
+import ChatResponse from './components/ChatResponse';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-primary text-center text-capitalize text-white'>
-      <h1>gemini ai chat bot</h1>
-    </div>
+    
+      <div className="App">
+        <header className="bg-primary text-center text-capitalize text-white">
+          <h1>gemini ai chat bot</h1>
+        </header>
+        <div>
+          <ChatRequest />
+          <ChatResponse />
+        </div>
+      </div>
+    
   );
 }
 
--- src/components/ChatRequest.jsx
+++ src/components/ChatRequest.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function ChatRequest() {
+export default function ChatRequest({onSubmit}) {
+    const [question,setQuestion] = useState("");
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    if(question.trim()) {
+        onSubmit(question);
+        setQuestion("");
+    }
+  };
   return (
-    <div>ChatRequest</div>
-  )
+    <div className="container my-4">
+      <div onSubmit={handelSubmit}>
+        <div class="form-group">
+          <label htmlFor="question" class="form-label">
+            Ask a Question
+          </label>
+          <input
+            type="text"
+            name="question"
+            id="question"
+            className="form-control"
+            placeholder="Enter your Question"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mt-2">
+            Submit
+
+        </button>
+      </div>
+    </div>
+  );
 }
