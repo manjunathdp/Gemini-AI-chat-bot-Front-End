3b0d44335c9137273d20699e911c4a8ed0ceaaf2
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-29 21:07:01 +0530

    components template added

---
+++ src/components/ChatRequest.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ChatRequest() {
+  return (
+    <div>ChatRequest</div>
+  )
+}
---
+++ src/components/ChatResponse.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ChatResponse() {
+  return (
+    <div>ChatResponse</div>
+  )
+}
