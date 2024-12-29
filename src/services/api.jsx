b2377dc968ada16de2f9eaa89400e02fa19244c0
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-29 23:52:08 +0530

    Api url corrected

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/ai/ask";
 
-export const fetchChatResponse = async(question) => {
-    try {
-        const response = await axios.post(API_URL,{question});
-        return response.data;
-    }catch(error) {
-        console.error(error);
-        throw error;
-    }    
-}
+export const fetchChatResponse = async (question) => {
+  try {
+    const response = await axios.post(API_URL, { question });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
